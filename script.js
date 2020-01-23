b38e1e4d60b9438c5ac44a2bbaa9acd775cd3370
Olivia Letendart <letendartolivia@gmail.com>
2020-01-23 16:11:52 +0100

    create let lastCheked + create foor if

--- script.js
+++ script.js
@@ -2,8 +2,15 @@
 
 const checkboxes = document.querySelectorAll('.inbox input[type="checkbox"]');
 
+let lastChecked;
+
 function handleCheck(e) {
-  console.log(e);
+  // Check if they had the shift key down
+  // AND check that they are checking it
+  if (e.shiftKey && this.checked) {
+    // go ahead and do what we please
+  }
+  lastChecked = this;
 }
 
 checkboxes.forEach(checkbox => checkbox.addEventListener("click", handleCheck));
